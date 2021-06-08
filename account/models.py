7f7de0976016d0730567a7a8a8f1731112da3357
yeazin <farsin310yeariha9701@gmail.com>
2021-06-08 22:08:21 +0600

    ch01

--- account/models.py
+++ account/models.py
@@ -6,6 +6,14 @@ import uuid
 class Quser(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    email = models.EmailField(unique=True, null=True)
+    profile_image = models.ImageField(upload_to='profile/', null=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_naem = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.email
+
 
     
 
--- account/views.py
+++ account/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+from django.utils.decorators import method_decorator
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+# model import
+from .models import Quser
 
-# Create your views here.
+# Dashboard View
+class Dashboard(View):
+    def get(self, request,*args,**kwargs):    
+        context={
+
+        }
+        return render(request,'account/dasboard/dashboard.html', context)
+
+# Register View
+class Register(View):
+    def get(self,request,*args,**kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return render (request,'account/user_info/register.html')
+    
+    def post(self,request,*args,**kwargs):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        f_name = request.POST.get('fname')
+        l_name = request.POST.get('lname')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        username_check = User.objects.filter(username=username)
+        email_check = Quser.objects.filter(email=email)
+        if username_check:
+            messages.warning(request,'Username Already Exitx!!')
+            # Redirect to same Page
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        elif password1 != password2:
+            messages.warning(request,'Password didnot Match')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        else:
+            auth_info ={
+                'username':username,
+                'password':make_password(password1)
+            }
+            user = User(**auth_info)
+            user.save()
+        if email_check:
+            messages.warning(request,'Email already Exits !!')
+            # redirect to same page
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+
+
