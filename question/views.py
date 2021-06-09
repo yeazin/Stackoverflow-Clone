13b9315d239a05494667aa943585fdd13db58b16
yeazin <farsin310yeariha9701@gmail.com>
2021-06-09 22:27:45 +0600

    ch10

--- question/views.py
+++ question/views.py
@@ -6,17 +6,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login,logout
 from django.utils.decorators import method_decorator
 from django.contrib import messages
+
+from question.models import Question
 # import from basis
 from .forms import QuestionForms
 
-# Create Questions
-class CreateQuestion(View):
-    def get(self,request,*args,**kwrargs):
-        q_forms = QuestionForms
-        context={
-            'forms':q_forms
-        }
-        return render(request,'question/create_q.html',context)
 
 # Questions view 
 class AllQuestions(View):
@@ -25,6 +19,17 @@ class AllQuestions(View):
 
         }
         return render (request,'question/q/questions.html', context)
+
+# View single Question 
+class SingleQuestion(View):
+    def get(self,request,id):
+        question_obj = get_object_or_404(Question,id=id)
+        question_obj.views_count = question_obj.views_count + 1
+        question_obj.save()
+        context={
+            'question':question_obj
+        }
+        return render(request,'question/q/single_question.html', context)
 
 # Tags View 
 class AllTags(View):
