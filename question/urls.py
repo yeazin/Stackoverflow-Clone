2cf0fce0a968c1282b42d2f61542c975cb5091db
Md. N. Islam Yeasin <43564198+yeazin@users.noreply.github.com>
2021-06-10 10:55:24 +0600

    Update urls.py

--- question/urls.py
+++ question/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('question/view/<str:id>', views.SingleQuestion.as_view(), name='single_question'),
 
     path('tags/', views.AllTags.as_view(), name='tags'),
+    path('upvote_tem/', views.upvote_template, name="upvote_tem"),
+    path('upvote/', views.upvote, name="upvote")
 ]
