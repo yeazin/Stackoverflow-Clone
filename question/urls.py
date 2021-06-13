ddc15ede7a7e1a680801dd8bf68bfdd1dcc45822
yeazin <farsin310yeariha9701@gmail.com>
2021-06-13 17:31:40 +0600

    ch30

--- question/urls.py
+++ question/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('question/view/<str:id>', views.SingleQuestion.as_view(), name='single_question'),
 
     path('tags/', views.AllTags.as_view(), name='tags'),
+    path('tagged/view/<str:id>', views.SingleTagView.as_view(), name='single_tag'),
     path('upvote_tem/', views.upvote_template, name="upvote_tem"),
     path('upvote/', views.upvote, name="upvote"),
     path('search_tag/', views.search_tag, name="search_tag"),
