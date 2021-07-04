5f8f6a03995ff32066972d3217a22f4a9a0d5672
yeazin <farsin310yeariha9701@gmail.com>
2021-07-04 15:14:05 +0600

    added super admin files

--- sadmin/views/dashboard.py
+++ sadmin/views/dashboard.py
@@ -1,4 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect, get_list_or_404
+from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
+
+class Dashboard(View):
+    def get(self,request,*args,**kwarsgs):
+        return HttpResponse("<h1> Hello guys</h1>")
